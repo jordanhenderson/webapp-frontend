304c551d7db42aaf8472dd6520ee4f1f38602707
Jordan Henderson <jordan.henderson@ioflame.com>
2014-01-08 15:06:29 +1000

    Replaced ` (backtick) with double quotes (SQL standard, works in MYSQL with pragma).

--- plugins/constants.lua
+++ plugins/constants.lua
@@ -1,8 +1,8 @@
 --Create database queries.
 @def (CREATE_DATABASE(db_type) {
-"BEGIN;",
-@join("CREATE TABLE `users` (`id` INTEGER PRIMARY KEY NOT NULL, `user` TEXT NOT NULL, `pass` TEXT NOT NULL, `salt` TEXT NOT NULL, `auth` INTEGER NOT NULL DEFAULT (",AUTH_USER,"), UNIQUE(`user`) ON CONFLICT IGNORE);"),
-"COMMIT;"
+'BEGIN;',
+@join('CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER PRIMARY KEY NOT NULL, "user" TEXT NOT NULL, "pass" TEXT NOT NULL, "salt" TEXT NOT NULL, "auth" INTEGER NOT NULL DEFAULT (',AUTH_USER,'), UNIQUE("user") ON CONFLICT IGNORE);'),
+'COMMIT;'
 })
 
 @def PRAGMA_FOREIGN "PRAGMA foreign_keys = ON;"
